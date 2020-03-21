5f81ce9e2e21239c7566907ecfc7dfc18af6ee2f
Samara <swondimu10@gmail.com>
2020-03-21 18:04:23 -0400

    Errors in code

--- FlashcardApp/Lab1README.md
+++ FlashcardApp/Lab1README.md
@@ -1,5 +1,28 @@
 ## Flashcard App
 
+## Lab 4
+
+### App Description
+This app transitions between flashcards smoothly and also has an app icon and splash screen.
+
+### App Walk-though
+
+<img src="YOUR_GIF_URL_HERE" width=200><br>
+
+`TODO://` In the User stories section below, add an `x` between the `-[ ]` like this `- [x]` for any user story you complete. (☝️ Remove this paragraph after after checking off user stories)
+
+## Required
+- [x] User should see the app icon 
+- [x] User should see a splash screen
+- [x] User should see an flip animation when revealing the answer
+- [x] User should see a slide animation when going to the next flashcard
+- [x] User should see a slide animation when going to the previous flashcard
+- [x] Push code to GitHub
+## Optional
+- [ ] User should see a nicer flaschard with rounded corners, shadows, and animating when showing the first time
+- [ ] User should see all views properly layout when using the app on a phone of different sizes
+- [ ] Correct answer in multiple choice should be random
+
 ## Lab 3
 
 ### App Description
--- FlashcardApp/ViewController.swift
+++ FlashcardApp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var card: UIView!
     
     var flashcards = [Flashcard]()
     var currentIndex = 0
@@ -80,26 +81,50 @@ class ViewController: UIViewController {
     @IBAction func didTapOnNext(_ sender: Any) {
         currentIndex = currentIndex + 1
         
-        updateLabels()
-        
         updateNextPrevButtons()
+        
+        animateCardOut()
     }
     
     @IBAction func didTapOnPrev(_ sender: Any) {
         currentIndex = currentIndex - 1
         
-        updateLabels()
-        
         updateNextPrevButtons()
+        
+        animateCardOut()
     }
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        if (questionLabel.isHidden == false){
-            questionLabel.isHidden = true
+       flipFlashcards()
+    }
+    
+    func flipFlashcards() {
+        
+    if (self.questionLabel.isHidden == false){
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {
+                self.questionLabel.isHidden = true
+        })
+    } else if (self.questionLabel.isHidden == true){
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromLeft, animations: {
+                self.questionLabel.isHidden = false
+            })
+    }
+    
+    func animateCardIn() {
+        card.transform = CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.card.transform = CGAffineTransform.identity
         }
-        else if (questionLabel.isHidden == true){
-            questionLabel.isHidden = false
-        }
+    }
+    
+    func animateCardOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)
+        }, completion: { finished in
+            self.updateLabels()
+            self.animateCardIn()
+        })
     }
     
     func updateFlashcard(question: String, answer: String) {
